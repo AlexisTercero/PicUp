21a1c817e11e06233048153dfbb9aceb2e8d47e8
alexistercero <alexisdealmeyda@gmail.com>
2022-08-10 10:18:30 -0300

    add multer

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const multer = require('multer'); //requiero multer,middleware maneja data para subirla.
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Welcome to PictureThis!');
+});
+
+router.post('/images/post', (req, res) => {
+  console.log(req.files);
 });
 
 module.exports = router;
